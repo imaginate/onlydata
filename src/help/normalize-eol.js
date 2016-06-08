e7f9154d4dbad5e669dc64cfbc6e479c7a9c8cbb
Adam Smith <adam@imaginate.life>
2016-06-07 19:10:36 -0700

    Added normalizeEol helper

---
+++ src/help/normalize-eol.js
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+var remap = require('./vitals').remap;
+
+/**
+ * @param {string} content
+ * @return {string}
+ */
+module.exports = function normalizeEol(content) {
+  return remap(content, /\r\n?/g, '\n');
+};
