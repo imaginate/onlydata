60712913231c7ed52b91521e25a47fa4c7a33e15
Adam Smith <adam@imaginate.life>
2016-06-08 21:32:31 -0700

    Added isBoolean parse method

---
+++ src/parse/boolean/is.js
@@ -0,0 +1,27 @@
+/**
+ * -----------------------------------------------------------------------------
+ * OnlyData
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [OnlyData](http://onlydata.tech)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+var has = require('../../help/vitals').has;
+
+var RE = /^(?:true|false|yes|no)(?:[ \t\v]+#.*)?$/i;
+
+/**
+ * @param {string} line
+ * @return {boolean}
+ */
+module.exports = function isBoolean(line) {
+  return has(line, RE);
+};
